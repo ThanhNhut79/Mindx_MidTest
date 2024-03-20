401c90bb750dfc9734fbcfcd512880f0d245d791
ThanhNhut79 <thanhnhutvn77@gmail.com>
2024-03-21 03:54:35 +0700

    first commit

--- app.js
+++ app.js
@@ -5,23 +5,15 @@ import userRoutes from "./src/Routes/userRoutes.js";
 import profileRoutes from "./src/Routes/profileRoutes.js";
 import authMiddleware from "./src/Middlewares/authenMiddleware.js";
 
-// Load environment variables from a .env file
-
-// Initialize Express app
 const app = express();
 
-// Connect to MongoDB database
 const uri = `mongodb+srv://nhut03:123@cluster0.cm7cwus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Middleware
 app.use(express.json());
 
-// Routes
-// app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/profile", authMiddleware, profileRoutes);
 
-// Connect to MongoDB database
 mongoose
   .connect(uri)
   .then(() => console.log("Connected to database successfully"))
